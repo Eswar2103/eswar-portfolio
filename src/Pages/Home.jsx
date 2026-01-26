7c3878931a55f9aa1e59b7bcc996bf4b9f9174d8
Guru Eswar sainath Reddy Kummithi <kummithi.sai00@gmail.com>
2026-01-26 21:23:23 +0530

    Projects and freelancing pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import Career from "../components/Career";
+import FreeLancing from "../components/FreeLancing";
 import Intro from "../components/Intro";
+import Projects from "../components/Projects";
 
 function Home() {
   return (
     <>
       <Intro />
       <Career />
+      <Projects />
+      <FreeLancing />
     </>
   );
 }
---
+++ src/components/FreeLancing.jsx
@@ -0,0 +1,30 @@
+import { ProjectItem, Projectsgrid } from "./Utils";
+
+function FreeLancing() {
+  let data = [
+    {
+      title:
+        "Freelance Frontend Developer – Client Portfolio Website (Confidential)",
+      description: [
+        "Designed and developed a responsive portfolio website for a public figure using React and modern UI practices.",
+        "Built sections for biography, media, achievements and public engagement.",
+        "Implemented SEO-friendly structure and performance-optimized layouts to improve visibility and loading speed.",
+        "Collaborated directly with the client to gather requirements and deliver a custom visual identity.",
+        "Built a protected admin interface using JWT token–based authentication, enabling secure and automated data uploads for gallery, news, and events management.",
+        "Deployed the application using AWS (S3, API Gateway, Lambda, DynamoDB, CloudFront) with CI-based automated deployment.",
+      ],
+      techStack: "React, Tailwind CSS, Redux, React Router, OAuth 2.0/OIDC",
+    },
+  ];
+  return (
+    <div className="mt-10 flex flex-col justify-center items-center gap-5">
+      <div className="text-teal-400 flex items-center gap-x-2 border border-stone-600 bg-black/40 px-2 py-1 rounded-lg text-lg uppercase font-bold tracking-widest">
+        <img src="./freelancing.png" className="w-9 h-8" />
+        <p className="tracking-wider">Freelancing</p>
+      </div>
+      <Projectsgrid projects={data} />
+    </div>
+  );
+}
+
+export default FreeLancing;
